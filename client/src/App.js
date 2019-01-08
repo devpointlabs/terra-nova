3a410ab35dd4f1761d84d3465c844d7b697adbf0
Harlan Evans <harlanevans5@gmail.com>
2019-01-08 11:20:19 -0700

    Build out components with Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Navbar from './components/Navbar';
+import NoMatch from './components/NoMatch';
+import FetchUser from './components/FetchUser';
+import { Container, } from 'semantic-ui-react';
+import { Route, Switch, } from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <FetchUser>
+      <Container>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Container>
+    </FetchUser>
+  </Fragment>
+)
 
 export default App;
