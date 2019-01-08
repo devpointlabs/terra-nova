ddb3821b7dfcfa531496754d80a762d0422e0774
Harlan Evans <harlanevans5@gmail.com>
2019-01-08 11:32:14 -0700

    Build ProtectedRoute and devise sanitized params

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Register from './components/Register';
 import Navbar from './components/Navbar';
 import NoMatch from './components/NoMatch';
 import FetchUser from './components/FetchUser';
+import ProtectedRoute from './components/ProtectedRoute';
 import { Container, } from 'semantic-ui-react';
 import { Route, Switch, } from 'react-router-dom';
 import './App.css';
@@ -15,7 +16,7 @@ const App = () => (
     <FetchUser>
       <Container>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <ProtectedRoute exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route component={NoMatch} />
