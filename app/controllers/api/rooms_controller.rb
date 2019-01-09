9550ab860ab89e6a0095925abc51c05962006eda
Chelsea Allred <chelsdallred@gmail.com>
2019-01-09 15:20:01 -0700

    build out rooms controller

--- app/controllers/api/rooms_controller.rb
+++ app/controllers/api/rooms_controller.rb
@@ -1,16 +1,41 @@
 class Api::RoomsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_room, only: [:show, :update, :destroy]
+
   def index
+    render json: Room.all
   end
 
   def show
+    render json: @room
   end
 
   def create
+    room = Room.new(room_params)
+    if room.save
+      render json: room
+    else
+      render json: room.error
+    end
   end
 
   def update
+    if @room.update(room_params)
+      render json: @room
+    else 
+      render json: @room.error
   end
 
   def destroy
+    @room.destroy
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  def room_params
+      params.require(:room).permit(:room_type, :cost, :max_occupancy)
   end
 end
