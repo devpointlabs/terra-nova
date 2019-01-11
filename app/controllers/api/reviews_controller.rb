98ff33eb57580038d1c877609225b37d25f21efb
Chelsea Allred <chelsdallred@gmail.com>
2019-01-11 10:22:50 -0700

    build out reviews controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,16 +1,42 @@
 class Api::ReviewsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_review, only: [:show, :update, :destroy]
+
   def index
+    render json: Review.all
   end
 
   def show
+    render json: @review
   end
 
   def create
+    review = current_user.reviews.new(review_params)
+    if review.save
+      render json: review
+    else
+      render json: review.error
+    end
   end
 
   def update
+    if @review.update(review_params)
+      render json: @review
+    else 
+      render json: @review.error
+    end
   end
 
   def destroy
+    @review.destroy
+  end
+
+  private
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :title, :body)
   end
 end
