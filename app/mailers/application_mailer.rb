8e900e482d844a18a2693138ff039a202d4ba306
Jessica Anderson <jessie.leigh30@gmail.com>
2019-01-21 15:45:29 -0700

    mailer html and text files created

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,14 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def welcome_email(user)
+    @user = user
+    # @url = 'Our domain name with login info'
+    mail(to: @user.email, subject: 'Welcome to Terra Nova Cabins!')
+  end
+
+  def reservation_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Your Reservation with Terra Nova Cabins')
 end
