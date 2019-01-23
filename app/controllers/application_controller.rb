ffe3988da9f0f51e309ca34b60742038aea75409
Crys <sovereigncrys88@gmail.com>
2019-01-23 15:04:00 -0700

    edited static controller again added RAILS::

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,7 @@
-require 'rails/application_controller'
-
-class ApplicationController < Rails::ActionController::API
+class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  # layout false
-
-  def index
-    render file: Rails.root.join('public', 'index.html')
-  end
-
 protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
