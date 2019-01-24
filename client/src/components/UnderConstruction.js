cba7f4ad205060bb61d848c6d89b5eac331a55bc
Jessica Anderson <jessie.leigh30@gmail.com>
2019-01-23 21:59:56 -0700

    translations added to under construction page

--- client/src/components/UnderConstruction.js
+++ client/src/components/UnderConstruction.js
@@ -1,20 +1,22 @@
 import React from "react";
-import LPI4 from "../assets/images/LandingPage/LPI4.jpg"
+import LPI4 from "../assets/images/LandingPage/LPI4.jpg";
+import { withNamespaces } from 'react-i18next';
 
 
 class UnderConstruction extends React.Component {
   render() {
+  const { t } = this.props;
     return (
       <div style={styles.background}>
-        <h2 style={styles.title2}> Coming Soon! </h2>
-        <h1 style={styles.title}> The Adventure Begins...</h1>
-        <h6 style={styles.body}> Hold tight as we get this page up and running. </h6>
+        <h2 style={styles.title2}> {t("Coming Soon")}! </h2>
+        <h1 style={styles.title}> {t("The Adventure Begins")}...</h1>
+        <h6 style={styles.body}> {t("Hold tight as we get this page up and running")}. </h6>
       </div>
     );
   }
 }
 
-export default UnderConstruction;
+export default withNamespaces()(UnderConstruction);
 
 const styles = {
   title: {
@@ -43,9 +45,9 @@ const styles = {
     // textShadow: "0.5px 0.5px #000000",
     textAlign: "center",
   },
-    // background: {
-    //   backgroundColor: "grey",
-    //   minHeight: "100%",
-    //   minWidth: "100%",
-    // }
+    background: {
+      backgroundImage: "grey",
+      minHeight: "100%",
+      minWidth: "100%",
+    }
 }
