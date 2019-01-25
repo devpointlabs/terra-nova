d29b8137563e1cddf2fbffd505585585ace636ec
Emilio <em.francomartinez@gmail.com>
2019-01-24 17:07:10 -0700

    merge conflict resolved again

--- client/src/App.js
+++ client/src/App.js
@@ -21,12 +21,9 @@ import Rooms from "./components/Rooms";
 import News from "./components/News";
 import Reviews from "./components/Reviews";
 import ResCheckout from "./components/ResCheckout";
-<<<<<<< HEAD
 import CheckoutConfirm from "./components/CheckoutConfirm";
-=======
 import UnderConstruction from "./components/UnderConstruction";
-import ScrollToTop from './components/ScrollToTop';
->>>>>>> 506198b24721a5a77d12a290975164a44827982f
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => (
   <Fragment>
@@ -34,35 +31,34 @@ const App = () => (
       <Geobar />
       <Navbar />
       <ScrollToTop>
-
-      <div style={styles.body}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <ProtectedRoute exact path="admin/home" component={Home} />
-          <Route exact path="/admin" component={Admin} />
-          <ProtectedRoute exact path="/register" component={Register} />
-          <Route exact path="/rooms" component={Rooms} />
-          <Route exact path="/about_us" component={AboutUs} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/gallery" component={Gallery} />
-          <Route exact path="/reservations" component={Reservations} />
-          <Route exact path="/checkout" component={ResCheckout} />
-          <Route
-            exact
-            path="/checkout/confirmation"
-            component={CheckoutConfirm}
-          />
-          <Route exact path="/events" component={Events} />
-          <Route exact path="/news" component={News} />
-          <Route exact path="/careers" component={Careers} />
-          <Route exact path="/rooms" component={Rooms} />
-          <ProtectedRoute path="/careerapply" component={CareerForm} />
-          <Route path="/reviewform" component={ReviewForm} />
-          <Route path="/reviews" component={Reviews} />
-          <Route path="/underconstruction" component={UnderConstruction} />
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
+        <div style={styles.body}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <ProtectedRoute exact path="admin/home" component={Home} />
+            <Route exact path="/admin" component={Admin} />
+            <ProtectedRoute exact path="/register" component={Register} />
+            <Route exact path="/rooms" component={Rooms} />
+            <Route exact path="/about_us" component={AboutUs} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/gallery" component={Gallery} />
+            <Route exact path="/reservations" component={Reservations} />
+            <Route exact path="/checkout" component={ResCheckout} />
+            <Route
+              exact
+              path="/checkout/confirmation"
+              component={CheckoutConfirm}
+            />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/news" component={News} />
+            <Route exact path="/careers" component={Careers} />
+            <Route exact path="/rooms" component={Rooms} />
+            <ProtectedRoute path="/careerapply" component={CareerForm} />
+            <Route path="/reviewform" component={ReviewForm} />
+            <Route path="/reviews" component={Reviews} />
+            <Route path="/underconstruction" component={UnderConstruction} />
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
       </ScrollToTop>
       <Footer style={styles.footer} />
     </FetchUser>
