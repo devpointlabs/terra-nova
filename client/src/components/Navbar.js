2117b97199f797c3f576cd8949fd1d9e68e23ed0
Alex Winitzky <alex.winitzky@gmail.com>
2019-01-28 11:48:22 -0700

    update admin navbar, handleregister

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { AuthConsumer } from "../providers/AuthProvider";
-import { Menu, Container, Image, Dropdown } from "semantic-ui-react";
+import { Menu, Container, Image, } from "semantic-ui-react";
 import { Link, withRouter } from "react-router-dom";
-// import ReactWeather from 'react-open-weather';
 import Terra_Nova_Cabins_Logo from "../assets/images/Terra_Nova_Cabins_Logo.png";
 import { withNamespaces } from "react-i18next";
 
@@ -22,46 +21,8 @@ class Navbar extends React.Component {
           verticalAlign="top"
           stackable
           />
-          </Link>
+        </Link>
       </Menu.Item>
-    );
-  };
-
-  adminNav = () => {
-    const { auth: { user, handleLogout }, t
-    } = this.props;
-
-    return (
-
-      // <Container>
-      <Dropdown.Menu style={styles.font} position="right"stackable>
-        {user ? (
-          <div style={styles.background}>
-            <Dropdown item style={styles.font} text={t("Welcome")}stackable>
-              <Link to="/register">
-                <Dropdown.Item
-                  style={styles.font}
-                  position="right"
-                  text={t("NEW ADMIN")}
-                  stackable
-                />
-              </Link>
-              <Dropdown.Item
-                href="/events"
-                style={styles.font}
-                text={t("NEW EVENT")}
-                stackable
-              />
-              <Dropdown.Item
-                style={styles.font}
-                text={t("LOGOUT")}
-                stackable
-                onClick={() => handleLogout(this.props.history)}
-              />
-            </Menu>
-          </div>
-        ) : null}
-      </Menu>
     );
   };
 
@@ -70,70 +31,168 @@ class Navbar extends React.Component {
   };
 
   render() {
-    const { location, t } = this.props;
-
-    return (
-      <div style={styles.background}>
-        <Container stackable>
-          <Menu position="center" secondary>
-            {this.renderIcon()}
-            <Link to="/">
+    const { auth: { user }, location, t } = this.props;
+    if (user) {
+      return (
+        <div style={styles.background}>
+          <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            color: 'white',
+            marginLeft: '50px',
+            marginRight: '-100px'
+          }}>
+            <h6 >{t("Welcome")},</h6>
+            <h5 style={{marginTop: '-5px'}}>{user.first_name}</h5>
+          </div>
+          <Container>
+            <Menu position="center" secondary>
+              {this.renderIcon()}
+              <Link to="/">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("HOME")}
+                  id="home"
+                  active={location.pathname === "/"}
+                />
+              </Link>
+              <Link to="/rooms">
+                <Menu.Item
+                  name="Room & Rate"
+                  id="Room & Rate"
+                  style={styles.adminFont}
+                  active={location.pathname === "/rooms"}
+                >
+                  {" "}
+                  {t("ROOM & RATE")}
+                </Menu.Item>
+              </Link>
+              <Link to="/reservations" onClick={this.handleRefresh}>
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("RESERVATIONS")}
+                  id="reservations"
+                  active={this.props.location.pathname === "/reservations"}
+                />
+              </Link>
+              <Link to="/gallery">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("GALLERY")}
+                  id="gallery"
+                  active={this.props.location.pathname === "/gallery"}
+                />
+              </Link>
+              <Link to="/about_us">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("ABOUT")}
+                  id="about"
+                  active={this.props.location.pathname === "/about_us"}
+                />
+              </Link>
+              <Link to="/contact">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("CONTACT")}
+                  id="contact"
+                  active={this.props.location.pathname === "/contact"}
+                />
+              </Link>
+              <Link to="/register">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("NEW ADMIN")}
+                  active={location.pathname === '/register'}
+                />
+              </Link>
+              <Link to="/events">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("NEW EVENT")}
+                  active={location.pathname === '/events'}
+                />
+              </Link>
+              <Link to="/reviews">
+                <Menu.Item
+                  style={styles.adminFont}
+                  name={t("REVIEWS")}
+                  active={location.pathname === '/reviews'}
+                />
+              </Link>
               <Menu.Item
-                style={styles.font}
-                name={t("HOME")}
-                id="home"
-                active={location.pathname === "/"}
+                style={{marginTop: '35px', color: 'white', fontWeight: 'bold'}}
+                name={t("LOGOUT")}
+                onClick={() => this.props.auth.handleLogout(this.props.history)}
               />
-            </Link>
-            <Link to="/rooms">
-              <Menu.Item
-                name="Room & Rate"
-                id="Room & Rate"
-                style={styles.font}
-                active={location.pathname === "/rooms"}
-              >
-                {" "}
-                {t("ROOM & RATE")}
-              </Menu.Item>
-            </Link>
-            <Link to="/reservations" onClick={this.handleRefresh}>
-              <Menu.Item
-                style={styles.font}
-                name={t("RESERVATIONS")}
-                id="reservations"
-                active={this.props.location.pathname === "/reservations"}
-              />
-            </Link>
-            <Link to="/gallery">
-              <Menu.Item
-                style={styles.font}
-                name={t("GALLERY")}
-                id="gallery"
-                active={this.props.location.pathname === "/gallery"}
-              />
-            </Link>
-            <Link to="/about_us">
-              <Menu.Item
-                style={styles.font}
-                name={t("ABOUT")}
-                id="about"
-                active={this.props.location.pathname === "/about_us"}
-              />
-            </Link>
-            <Link to="/contact">
-              <Menu.Item
-                style={styles.font}
-                name={t("CONTACT")}
-                id="contact"
-                active={this.props.location.pathname === "/contact"}
-              />
-            </Link>
-            {this.adminNav()}
-          </Menu>
-          <br />
-        </Container>
-      </div>
-    );
+            </Menu>
+            <br />
+          </Container>
+        </div>
+      )
+    } else {
+      return (
+        <div style={styles.background}>
+          <Container>
+            <Menu position="center" secondary>
+              {this.renderIcon()}
+              <Link to="/">
+                <Menu.Item
+                  style={styles.font}
+                  name={t("HOME")}
+                  id="home"
+                  active={location.pathname === "/"}
+                />
+              </Link>
+              <Link to="/rooms">
+                <Menu.Item
+                  name="Room & Rate"
+                  id="Room & Rate"
+                  style={styles.font}
+                  active={location.pathname === "/rooms"}
+                >
+                  {" "}
+                  {t("ROOM & RATE")}
+                </Menu.Item>
+              </Link>
+              <Link to="/reservations" onClick={this.handleRefresh}>
+                <Menu.Item
+                  style={styles.font}
+                  name={t("RESERVATIONS")}
+                  id="reservations"
+                  active={this.props.location.pathname === "/reservations"}
+                />
+              </Link>
+              <Link to="/gallery">
+                <Menu.Item
+                  style={styles.font}
+                  name={t("GALLERY")}
+                  id="gallery"
+                  active={this.props.location.pathname === "/gallery"}
+                />
+              </Link>
+              <Link to="/about_us">
+                <Menu.Item
+                  style={styles.font}
+                  name={t("ABOUT")}
+                  id="about"
+                  active={this.props.location.pathname === "/about_us"}
+                />
+              </Link>
+              <Link to="/contact">
+                <Menu.Item
+                  style={styles.font}
+                  name={t("CONTACT")}
+                  id="contact"
+                  active={this.props.location.pathname === "/contact"}
+                />
+              </Link>
+            </Menu>
+            <br />
+          </Container>
+        </div>
+      );
+    }
   }
 }
 
@@ -160,6 +219,18 @@ export const styles = {
     alignItems: "center",
     alignContent: "center",
     marginTop: "35px"
+  },
+  adminFont: {
+    fontFamily: "'Poppins', sans-serif",
+    color: "white",
+    fontWeight: "bold",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    alignContent: "center",
+    marginTop: "35px",
+    marginBottom: '-20px'
   },
   image: {
     padding: "0.5px",
--- client/src/index.js
+++ client/src/index.js
@@ -10,15 +10,20 @@ import "./i18n";
 import { Provider } from "react-redux";
 import store from "./store";
 import * as serviceWorker from './serviceWorker';
+import { initMiddleware, } from 'devise-axios';
+
+initMiddleware()
 
 ReactDOM.render(
-  <Provider store={store}>
-    <AuthProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </AuthProvider>
-  </Provider>,
+  <AuthProvider>
+    <React.Fragment>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </React.Fragment>
+  </AuthProvider>,
   document.getElementById("root")
 );
 
